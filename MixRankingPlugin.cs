847c678e8916f72f3073d20bc1b4a4014fc19d27
marcus.toledo <marcus.toledo@ciss.com.br>
2026-08-01 14:00:49 -0300

    fix: store HttpWebSyncClient in a field for later disposal

--- MixRankingPlugin.cs
+++ MixRankingPlugin.cs
@@ -25,6 +25,7 @@ public class MixRankingPlugin : BasePlugin, IPluginConfig<RankingConfig>
 
     private DatabaseService _database = null!;
     private StatisticsService _statistics = null!;
+    private HttpWebSyncClient _webSyncClient = null!;
     private WebSyncService _webSyncService = null!;
     private RatingService _ratingService = null!;
     private PlayerService _playerService = null!;
@@ -59,8 +60,8 @@ public class MixRankingPlugin : BasePlugin, IPluginConfig<RankingConfig>
 
         // 2. Initialize services
         _statistics = new StatisticsService();
-        var httpClient = new HttpWebSyncClient(Config);
-        _webSyncService = new WebSyncService(_database, httpClient, Logger);
+        _webSyncClient = new HttpWebSyncClient(Config);
+        _webSyncService = new WebSyncService(_database, _webSyncClient, Logger);
         _ratingService = new RatingService(_database, Config, _webSyncService);
         _playerService = new PlayerService(_database, Config.InitialRating);
         _matchService = new MatchService(_statistics, _ratingService, Config, Logger);
